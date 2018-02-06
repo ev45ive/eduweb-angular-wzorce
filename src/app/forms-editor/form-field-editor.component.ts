ba4acc850793da653400b95ea20c3bc4301116af
Mateusz Kulesza <eduwebgit@gmail.com>
2018-02-06 14:07:33 +0100

    4.2. Pola checkbox i radio z ngModel - finished

--- src/app/forms-editor/form-field-editor.component.ts
+++ src/app/forms-editor/form-field-editor.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
         <label>Field Name</label>
         <input type="text" class="form-control" [(ngModel)]="field_data.name">
       </div>
-
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" [(ngModel)]="field_data.active"> Active
+        </label>
+      </div>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="radio" [value]="true" [(ngModel)]="field_data.enabled"> Enabled 
+        </label>
+      </div>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="radio" [value]="false" [(ngModel)]="field_data.enabled"> Disabled 
+        </label>
+      </div>
+    <hr>
       <pre>Model: {{ field_data | json }}</pre>
     </div>
   `,
@@ -17,7 +32,9 @@ import { Component, OnInit } from '@angular/core';
 export class FormFieldEditorComponent implements OnInit {
 
   field_data = {
-    name: 'Default value'
+    name: 'Default value',
+    active: true,
+    enabled: true,
   }
 
   constructor() { }
