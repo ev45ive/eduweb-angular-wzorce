3bd868d7a479f7b9361b47ec1e473c706b637082
Mateusz Kulesza <eduwebgit@gmail.com>
2018-02-26 20:19:03 +0100

    2.1. Wzorzec wstrzykiwania zaleznosci - finished

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,53 @@
 
+class DataSource{
+  data
+  constructor(private config){
+    this.data = ' data from ' + config.sourceUrl
+  }
+}
+
+class MockDataSource extends DataSource{
+  data = 'test data'
+  constructor(config){
+    super(config)
+  }
+}
+
 class ComponentA{
 
-  constructor(){
-    console.log('>>> Getting some data')
+  constructor(private dataSource){
+    console.log('>>> Getting ' + this.dataSource.data)
   }
 }
 
 
-const cmp  = new ComponentA()
+/// app.ts
+class App{
+  getApiUrl(){
+    return 'http://some.real.server/'
+  }
+  
+  getDataSource(api_url){
+    return new DataSource({
+      sourceUrl: api_url
+    })
+  }
+
+  getComponent(dataSource){
+    new ComponentA(dataSource)
+  }
+
+  constructor(){
+    this.getComponent(this.getDataSource(this.getApiUrl()))
+  }
+}
+new App()
+
+/// test.ts
+class Test extends App{
+  
+  getDataSource(api_url){
+    return new MockDataSource({})
+  }
+}
+new Test()
