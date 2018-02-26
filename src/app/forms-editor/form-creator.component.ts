adf451a217d1a6c4412399de34183c38f5712361
Mateusz Kulesza <eduwebgit@gmail.com>
2018-02-26 12:48:13 +0100

    formgroup, formControl, formArray

--- src/app/forms-editor/form-creator.component.ts
+++ src/app/forms-editor/form-creator.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'form-creator',
   template: `
-    <p>
-      form-creator works!
-    </p>
+  <form [formControl]="formCreator">
+      <div class="form-group">
+        <label>Form name</label>
+        <input [formControl]="formTitle" name="title" class="form-control" />
+      </div>  
+
+      <button (click)="add()">add</button>
+
+      <pre>{{formCreator.value | json }} </pre>
+    </form>
   `,
   styles: []
 })
 export class FormCreatorComponent implements OnInit {
 
-  constructor() { }
+  formTitle: FormControl
+  formCreator: FormGroup
+  formFields: FormArray
+
+  constructor() {
+    this.formTitle = new FormControl('')
+    this.formFields = new FormArray([])
+        this.formCreator = new FormGroup({
+          'title':this.formTitle,
+          'fields': this.formFields
+        })
+    this.formFields.push(new FormControl(''))
+
+    console.log(this.formTitle)
+  }
+
+  add() {
+    this.formFields.push(new FormControl(''))
+  }
 
   ngOnInit() {
   }
--- src/app/forms-editor/forms-editor.module.ts
+++ src/app/forms-editor/forms-editor.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsEditorComponent } from './forms-editor.component';
 import { FormFieldEditorComponent } from './form-field-editor.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormCreatorComponent } from './form-creator.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [FormsEditorComponent, FormFieldEditorComponent, FormCreatorComponent],
   exports: [FormsEditorComponent]
