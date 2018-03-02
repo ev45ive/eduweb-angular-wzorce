35b324ea43b9d2edde40de9ebece12563652ada7
Mateusz Kulesza <eduwebgit@gmail.com>
2018-03-02 13:21:12 +0100

    2.5. Hierarchia wstrzykiwania - finished

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { Injector, Injectable, Inject, ReflectiveInjector, InjectionToken } from "@angular/core";
 
+export const DATASOURCE_MAIN_CONFIG = new InjectionToken('Data Source Main Configuration')
 export const DATASOURCE_CONFIG = new InjectionToken('Data Source Configuration')
 
 @Injectable()
-class DataSource{
+class DataSource {
   data
-  constructor(@Inject(DATASOURCE_CONFIG) private config){
+  constructor( @Inject(DATASOURCE_CONFIG) private config) {
     this.data = ' data from ' + config.sourceUrl
   }
 }
 
 @Injectable()
-class ComponentA{
+class ComponentA {
   message = 'welcome message'
 
-  constructor(@Inject(DataSource) private dataSource){
+  constructor( @Inject(DataSource) private dataSource) {
     console.log('>>> Getting ' + this.dataSource.data)
   }
 }
@@ -27,6 +28,15 @@ const injector = ReflectiveInjector.resolveAndCreate([
     }
   },
   DataSource,
-  ComponentA
 ])
-console.log(injector.get(ComponentA))
+
+const childInjector = ReflectiveInjector.resolveAndCreate([
+  ComponentA
+], injector)
+
+const childInjector2 = ReflectiveInjector.resolveAndCreate([
+  ComponentA
+], injector)
+
+console.log(childInjector.get(ComponentA))
+console.log(childInjector2.get(ComponentA))
