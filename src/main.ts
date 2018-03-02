a3419071f53fe70860f290e460efa38e7c93c910
Mateusz Kulesza <eduwebgit@gmail.com>
2018-03-02 13:54:05 +0100

    2.6. Usługi typu Singleton - start

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,12 @@
-import { Injector, Injectable, Inject, ReflectiveInjector, InjectionToken } from "@angular/core";
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-export const DATASOURCE_MAIN_CONFIG = new InjectionToken('Data Source Main Configuration')
-export const DATASOURCE_CONFIG = new InjectionToken('Data Source Configuration')
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-@Injectable()
-class DataSource {
-  data
-  constructor( @Inject(DATASOURCE_CONFIG) private config) {
-    this.data = ' data from ' + config.sourceUrl
-  }
+if (environment.production) {
+  enableProdMode();
 }
 
-@Injectable()
-class ComponentA {
-  message = 'welcome message'
-
-  constructor( @Inject(DataSource) private dataSource) {
-    console.log('>>> Getting ' + this.dataSource.data)
-  }
-}
-
-const injector = ReflectiveInjector.resolveAndCreate([
-  {
-    provide: DATASOURCE_CONFIG,
-    useValue: {
-      sourceUrl: 'http://some.real.server/'
-    }
-  },
-  DataSource,
-])
-
-const childInjector = ReflectiveInjector.resolveAndCreate([
-  ComponentA
-], injector)
-
-const childInjector2 = ReflectiveInjector.resolveAndCreate([
-  ComponentA
-], injector)
-
-console.log(childInjector.get(ComponentA))
-console.log(childInjector2.get(ComponentA))
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
