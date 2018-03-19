cbf832eb3c1249c2eab8443da674d8a2f6bd69a4
Mateusz Kulesza <eduwebgit@gmail.com>
2018-03-19 15:59:13 +0100

    dynamic multitype field editors

--- src/app/forms-editor/form-creator.component.ts
+++ src/app/forms-editor/form-creator.component.ts
@@ -4,16 +4,45 @@ import { FormControl, FormArray, FormGroup } from '@angular/forms';
 @Component({
   selector: 'form-creator',
   template: `
-  <form [formControl]="formCreator">
-      <div class="form-group">
-        <label>Form name</label>
-        <input [formControl]="formTitle" name="title" class="form-control" />
-      </div>  
+  <div class="row">
+    <div class="col">
+      <form [formGroup]="formCreator">
+        <div class="form-group">
+          <label>Form name</label>
+          <input [formControl]="formTitle" class="form-control" />
+        </div>  
 
-      <button (click)="add()">add</button>
+        <h4>Fields</h4>
+        <div class="border rounded mb-2"  *ngFor="let field of formFields">
+          <span class="close float-right p-2">&times;</span>
+          <div class="form-group px-4">
+            <label>Label</label>
+            <input [formControl]="field.get('label')" class="form-control" />
+          </div>  
+          <div class="form-group px-4" *ngIf="field.get('type').value == 'select'">
+            <label>Options</label>
+            <input *ngFor="let option of field.get('options').controls" [formControl]="field.get('options.0.value')" class="form-control" />
+          </div>  
+        </div>
 
+        <div class="input-group">
+          <button class="btn" (click)="add(typeSelect.value)">Add Field</button>
+          <select class="form-control" #typeSelect>
+            <option value="text" selected>Text Field</option>
+            <option value="select">Select Field</option>
+          </select>
+        </div>
+      </form>
+    </div>
+    <div class="col-4">
       <pre>{{formCreator.value | json }} </pre>
-    </form>
+
+      <div class="form-group" *ngFor="let field of formCreator.value.fields">
+        <label>{{field.label}}</label>
+        <input type="text" class="form-control">
+      </div>
+    </div>
+  </div>
   `,
   styles: []
 })
@@ -26,17 +55,35 @@ export class FormCreatorComponent implements OnInit {
   constructor() {
     this.formTitle = new FormControl('')
     this.formFields = new FormArray([])
-        this.formCreator = new FormGroup({
-          'title':this.formTitle,
-          'fields': this.formFields
-        })
-    this.formFields.push(new FormControl(''))
+    this.formCreator = new FormGroup({
+      'title': this.formTitle,
+      'fields': this.formFields
+    })
 
     console.log(this.formTitle)
   }
 
-  add() {
-    this.formFields.push(new FormControl(''))
+  add(type) {
+    switch (type) {
+      case 'select':
+        this.formFields.push(new FormGroup({
+          type: new FormControl('select'),
+          label: new FormControl(),
+          options: new FormArray([
+            new FormGroup({
+              label: new FormControl(''),
+              value: new FormControl(''),
+            })
+          ])
+        }))
+        break;
+      case 'text':
+      default:
+        this.formFields.push(new FormGroup({
+          type: new FormControl('text'),
+          label: new FormControl()
+        }))
+    }
   }
 
   ngOnInit() {
