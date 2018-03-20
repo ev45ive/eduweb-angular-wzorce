ffaaf407c39406d34df45f68098c69cce3576ec3
Mateusz Kulesza <eduwebgit@gmail.com>
2018-03-20 15:23:34 +0100

    5.5. Zagniezdzone modele formularza - finished

--- src/app/forms-editor/form-creator.component.ts
+++ src/app/forms-editor/form-creator.component.ts
@@ -6,31 +6,50 @@ import { FormControl, FormGroup, AbstractControl, FormArray, FormBuilder } from 
   template: `
     <div class="row">
       <div class="col">
+      <form [formGroup]="formDefinition">
         <div class="form-group">
-          <input type="text" class="form-control" [formControl]="formTitle">
+          <input type="text" class="form-control" formControlName="title">
         </div>
 
-        <div class="form-group">
-          <label>Checkbox Options</label>
-          <div class="input-group" *ngFor="let option of optionsForField.controls" [formGroup]="option">
-            <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <input type="checkbox" class="form-check" formControlName="selected">
+        <div *ngFor="let field of getControlsList(this.formDefinition.get('fields'))" class="border rounded p-2 mb-2">
+          <div [ngSwitch]="field.get('type').value">
+
+            <div *ngSwitchCase=" 'text' ">
+              <h6>Text Field:</h6>
+              <div class="form-group" [formGroup]="field">
+                <label>Label:</label>
+                <input type="text" class="form-control" formControlName="label">
               </div>
             </div>
-            <input type="text" class="form-control" formControlName="value">
-          </div>
 
+            <div *ngSwitchCase=" 'options' ">
+              <h6>Options Field:</h6>
+              <div class="form-group" [formGroup]="field">
+                <label>Label:</label>
+                <input type="text" class="form-control" formControlName="label">
+              </div>
+              <div class="form-group">
+                <label>Checkbox Options</label>
+                <div class="input-group" *ngFor="let option of getControlsList(field.get('options'))" [formGroup]="option">
+                  <div class="input-group-prepend">
+                      <div class="input-group-text">
+                        <input type="checkbox" class="form-check" formControlName="selected">
+                    </div>
+                  </div>
+                  <input type="text" class="form-control" formControlName="value">
+                </div>
+              </div>
+  
+            </div>
+          </div>
         </div>
         
-        <div class="form-group" [formGroup]="fieldOptions">
-          <label>Label:</label>
-          <input type="text" class="form-control" formControlName="label">
-        </div>
+      </form>
+
+      
       </div>
       <div class="col-4">
-        <pre>{{fieldOptions.value | json }}</pre>
-        <pre>{{optionsForField.value | json }}</pre>
+        <pre>{{formDefinition.value | json }}</pre>
       </div>
     </div>
   `,
@@ -38,24 +57,43 @@ import { FormControl, FormGroup, AbstractControl, FormArray, FormBuilder } from 
 })
 export class FormCreatorComponent implements OnInit {
 
-  formTitle: FormControl
-  fieldOptions: FormGroup
-  optionsForField: FormArray
+  formDefinition: FormGroup
 
   constructor(private fb:FormBuilder) { 
 
-    this.formTitle = this.fb.control('batman')
+    this.formDefinition = this.fb.group({
+      title: this.fb.control(''),
+      fields: this.fb.array([
+        this.createTextField(),
+        this.createOptionsField()
+      ])
+    })
+  }
 
-    this.fieldOptions = this.fb.group({
+  createTextField(){
+    return this.fb.group({
       type: this.fb.control('text'),
       label: this.fb.control('')
     })
-  
-    this.optionsForField = this.fb.array([
-      this.createOption('Test 1'),
-      this.createOption('Test 2'),
-      this.createOption('Test 3'),
-    ])
+  }
+
+  createOptionsField(){
+    return this.fb.group({
+      type: this.fb.control('options'),
+      label: this.fb.control(''),
+      options: this.fb.array([
+        this.createOption('Test 1'),
+        this.createOption('Test 2'),
+        this.createOption('Test 3'),
+      ])
+    })
+  }
+
+  getControlsList(fields:AbstractControl){
+    if(!(fields instanceof FormArray)){
+      return []
+    }
+    return fields.controls
   }
 
   createOption(defaultValue, selected = false){
