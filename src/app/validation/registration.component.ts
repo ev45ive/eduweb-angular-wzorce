9da23f8ed69685494d63b32a3395b27904875e95
Mateusz Kulesza <eduwebgit@gmail.com>
2018-04-03 16:29:05 +0200

    6.3. Własny walidatory - finished

--- src/app/validation/registration.component.ts
+++ src/app/validation/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, ValidatorFn, ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'registration',
@@ -40,8 +40,12 @@ import { FormBuilder, Validators } from '@angular/forms';
           <div *ngIf="registrationForm.get('password').hasError('required')">
             Field is required
           </div>
-          <div *ngIf="registrationForm.get('password').hasError('pattern')">
-            Password has to contain Uppercase, Lowercase letters and number
+          <div *ngIf="registrationForm.get('password').getError('password') as error">
+            Password has to contain
+              <span *ngIf="error.lowercase"> lowercase letters </span>
+              <span *ngIf="error.uppercase"> uppercase letters </span>
+              <span *ngIf="error.number"> numbers </span>
+              <span *ngIf="error.special"> special characters </span>
           </div>
         </div>
       </div>
@@ -68,23 +72,68 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class RegistrationComponent implements OnInit {
 
   registrationForm = this.form.group({
-    username: this.form.control('',[
+    username: this.form.control('', [
       Validators.required,
       Validators.minLength(3)
     ]),
-    email: this.form.control('',[
+    email: this.form.control('', [
       Validators.required,
       Validators.email
     ]),
-    password: this.form.control('',[
+    password: this.form.control('', [
       Validators.required,
-      Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$')
+      this.validatePassword({
+        lowercase:true, 
+        uppercase:true,
+        number:true
+      })
+      // Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$')
     ]),
     repeat_password: this.form.control(''),
   })
 
-  constructor(private form:FormBuilder) { }
+  validatePassword(options:{
+    uppercase?:boolean;
+    lowercase?:boolean;
+    number?:boolean;
+    special?:boolean;
+  }):ValidatorFn{
   
+    return (control: FormControl) => {
+
+      const hasUppercase = control.value.match(/[A-Z]/)
+      const hasLowercase = control.value.match(/[a-z]/)
+      const hasNumber = control.value.match(/[\d]/)
+      const hasSpecial = control.value.match(/[\W]/)
+
+      const errors = {}
+      let valid = true
+
+      if(options.lowercase && !hasLowercase){
+        errors['lowercase'] = true
+        valid = false
+      }
+      if(options.uppercase && !hasUppercase){
+        errors['uppercase'] = true
+        valid = false
+      }
+      if(options.number && !hasNumber){
+        errors['number'] = true
+        valid = false
+      }
+      if(options.special && !hasSpecial){
+        errors['special'] = true
+        valid = false
+      }
+
+      return valid? null : {
+        'password': errors
+      }
+    }
+  }
+
+  constructor(private form: FormBuilder) { }
+
   ngOnInit() {
     console.log(this.registrationForm)
 
