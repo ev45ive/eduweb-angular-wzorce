1e8bd884577169c8ecdbf5cb5292dca2d5c80876
Mateusz Kulesza <eduwebgit@gmail.com>
2018-04-06 21:45:06 +0200

    7.4. Cache zapytań i ReplaySubject - finished

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -2,27 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { User } from './models/user';
 import { Subject } from 'rxjs/Subject'
-import {multicast, refCount, share } from 'rxjs/operators'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import { ReplaySubject } from 'rxjs/ReplaySubject'
+import {multicast, refCount, share, shareReplay } from 'rxjs/operators'
 import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProfileService {
 
   api_url = 'http://localhost:3000/users/'
 
-  user_request = this.http
-    .get<User>(this.api_url + '1')
-    .pipe(
-      // multicast(()=>new Subject<User>()),
-      // refCount()
-      share()
-    )
+  private user_request:Observable<User>
 
   getUserProfile(){
+
+    if(!this.user_request){
+      this.user_request = this.http
+      .get<User>(this.api_url + '1')
+      .pipe(
+        shareReplay()
+      )
+    }
+
     return this.user_request
   }
 
+  clearCache(){
+    this.user_request = null
+  }
+
   constructor(private http:HttpClient) {
+
+    // setInterval(()=>{
+    //   this.clearCache()
+    // },5000)
   }
 
 }
